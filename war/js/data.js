d5488020285475214ef3860f4328eaa7b605a7fc
Cengizism <culusoy@ask-cs.com>
2013-06-13 17:54:13 +0200

    sample data & step-by-step for buying number

---
+++ war/js/data.js
@@ -0,0 +1,26 @@
+
+var data_number_types = ['local', 'virtual', 'premium'];
+
+var data_countries = ['nederland'];
+
+var data_regions = ['010', '020', '030', '040', '050'];
+
+var data_virtual_types = [
+	'Personal assistant services',
+	'VPN',
+	'Elektronisch communicatie',
+	'Company numbers'
+];
+
+var data_subscriptions = [
+	{
+		title:		'5 euro per month',
+		content:	'Oneline Personal<br>Monthly<br>Subscription'
+	},
+	{
+		title:		'50 euro per year',
+		content:	'Oneline Personal<br>Yearly<br>Subscription'
+	}
+];
+
+var data_payment_methods = ['Credit Card', 'iDeal', 'PayPal'];
