9f786dbd2a7fa44dce49e55e956f34bb5d4dfd03
Cengizism <culusoy@ask-cs.com>
2013-10-29 14:39:10 +0100

    redirect to login in success callback of logout

--- app/scripts/controllers/logout.js
+++ app/scripts/controllers/logout.js
@@ -6,38 +6,14 @@ define(
 
     controllers.controller('logout',
       [
-        '$rootScope', '$scope', '$window', 'Session', 'User', 'Storage',
-        function ($rootScope, $scope, $window, Session, User, Storage)
+        '$rootScope', '$scope', '$window', 'Session', 'User', 'Storage', '$location',
+        function ($rootScope, $scope, $window, Session, User, Storage, $location)
         {
           // TODO: Lose jQuery stuff
           $('.navbar').hide();
           $('#footer').hide();
 
           var logindata = angular.fromJson(Storage.get('logindata'));
-
-          Storage.clearAll();
-
-          console.log('doc cookie before ->', document.cookie);
-
-          // document.cookie = "OneLine.session=''; expires=Thu, 01-Jan-1970 00:00:01 GMT";
-
-          var cookies = document.cookie.split(";");
-
-          for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i];
-            var eqPos = cookie.indexOf("=");
-            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            $window.document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-          }
-
-          console.log('doc cookie after ->', document.cookie);
-
-          Storage.session.clearAll();
-
-          if (logindata.remember)
-          {
-            Storage.add('logindata', angular.toJson(logindata));
-          }
 
           User.logout()
             .then(function (result)
@@ -46,9 +22,27 @@ define(
               {
                 console.warn('error ->', result);
               }
+
+              Storage.clearAll();
+
+              console.log('doc cookie before ->', document.cookie);
+
+              document.cookie = "OneLine.session=''; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+
+              console.log('doc cookie after ->', document.cookie);
+
+              Storage.session.clearAll();
+
+              if (logindata.remember)
+              {
+                Storage.add('logindata', angular.toJson(logindata));
+              }
+
+              $location.path('/login');
+
             });
 
-          $window.location.href = 'index.html';
+          // $window.location.href = 'index.html';
         }
       ]
     );
