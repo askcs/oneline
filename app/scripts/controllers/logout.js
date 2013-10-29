4d660bc02d67c598f2f0c43e8fb213b6a3da6f53
Cengizism <culusoy@ask-cs.com>
2013-10-29 14:12:05 +0100

    app main navigation icons changed based on designs

--- app/scripts/controllers/logout.js
+++ app/scripts/controllers/logout.js
@@ -27,7 +27,7 @@ define(
             var cookie = cookies[i];
             var eqPos = cookie.indexOf("=");
             var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            $window.document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
           }
 
           console.log('doc cookie after ->', document.cookie);
