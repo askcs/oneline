06384ebe4bb82b4be76d14e328dead1e031541e3
Cengizism <culusoy@ask-cs.com>
2013-10-29 13:21:39 +0100

    erase all subdomain cookies

--- app/scripts/controllers/logout.js
+++ app/scripts/controllers/logout.js
@@ -19,7 +19,16 @@ define(
 
           console.log('doc cookie before ->', document.cookie);
 
-          document.cookie = "OneLine.session=''; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+          // document.cookie = "OneLine.session=''; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+
+          var cookies = document.cookie.split(";");
+
+          for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            var eqPos = cookie.indexOf("=");
+            var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+          }
 
           console.log('doc cookie after ->', document.cookie);
 
